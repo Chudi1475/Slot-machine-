fed0f375bc6d601a8305c1322d6d74678cae44e7
Chudi1475 <chudid777@gmail.com>
2025-05-29 12:21:20 -0500

    Auto-commit: 2025-05-29 12:21:19

--- main.py
+++ main.py
@@ -35,3 +35,35 @@ def main():
 if __name__ == "__main__":
     main()
     
+def print_slot_machine(rows, cols, symbols):
+    for row in range(rows):
+        for col in range(cols):
+            random_symbol = random.choice(symbols)
+            print(random_symbol, end=" ")
+        print() 
+        
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.") 
+        else:
+            print("Please enter a valid number.")
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+    return lines
