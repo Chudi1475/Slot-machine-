ece1d3126e440411d33744f78e52dcdc1ea81082
Chudi1475 <chudid777@gmail.com>
2025-05-29 12:19:34 -0500

    Auto-commit: 2025-05-29 12:19:34

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def deposit():
             print("Please enter a valid number.")
             
     return amount
-            
-            
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+    return lines
 
