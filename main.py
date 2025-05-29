5cb3f736ca9c09066877b687708e9de94e44b5fa
Chudi1475 <chudid777@gmail.com>
2025-05-29 12:16:39 -0500

    Auto-commit: 2025-05-29 12:16:39

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-# slot machine game
-#auto commit set up
-
-#this way
-import random
-if __name__ == "__main__":
-    print("Welcome to the slot machine game!")
-    print("You can bet between 1 and 100 credits.")
-    print("Good luck!")
+def deposit():
     while True:
-        bet = int(input("How many credits do you want to bet? "))
-        if bet < 1 or bet > 100:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a valid number.")
+            
+            
 
